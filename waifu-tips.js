0b08627c95cb62fdf29636d40af3264f6b3b4c26
huangwb8 <654751191@qq.com>
2022-05-26 23:07:03 +0800

    Update waifu-tips.js

--- waifu-tips.js
+++ waifu-tips.js
@@ -203,7 +203,7 @@ function loadWidget(config) {
 				});
 				$("#live2d").mousedown(function(e) {
 				        if(e.which==3){
-				        showMessage("<div style=\"text-align:center\">秘密通道<br/><a href=\"javascript:aplayer_panel_toggle();\">音乐播放器</a><br/><a href=\"https://github.com/huangwb8/\" target=\"_blank\">Github</a><br/><a href=\"https://chevereto.hwb0307.com/\" target=\"_blank\">图床</a><br/><a href=\"https://umamirn2.hwb0307.com/share/QqhvkqI6/blognas/\" target=\"_blank\">流量</a><br/><a href=\"https://blognas.hwb0307.com/linux/docker/1165\" target=\"_blank\">说明书</a></div>",4000,10);
+				        showMessage("<div style=\"text-align:center\">秘密通道<br/><a href=\"javascript:aplayer_panel_toggle();\">音乐播放器</a><br/><a href=\"https://github.com/huangwb8/live2dyy/\" target=\"_blank\">Github</a><br/><a href=\"https://chevereto.hwb0307.com/\" target=\"_blank\">图床</a><br/><a href=\"https://umamirn2.hwb0307.com/share/QqhvkqI6/blognas/\" target=\"_blank\">流量</a><br/><a href=\"https://blognas.hwb0307.com/linux/docker/1165\" target=\"_blank\">说明书</a></div>",4000,10);
 						}
 				});
 				$("#live2d").bind("contextmenu", function(e) {
